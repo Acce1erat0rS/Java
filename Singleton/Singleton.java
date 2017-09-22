61875e1c4161f9ceda0be4f7eda0e71eb3a0e27d
Spica Liu <i@liutianyu.cn>
2017-09-22 13:27:48 +0800

    Singleton modified, making it able to run.

--- Singleton/Singleton.java
+++ Singleton/Singleton.java
@@ -1,13 +1,16 @@
 public class Singleton{
 	int value;
-	private Singleton sgt = new Singleton();
-	private Singleton(int value){
-		this.value = value;
-	}
+	private static Singleton sgt = new Singleton();
+	private Singleton(){}
 	public static Singleton getSingleton(){
 		return sgt;
 	}
 	public static void main(String[] args){
+		
 		Singleton sgt = Singleton.getSingleton();
+		Singleton sgt2 = Singleton.getSingleton();
+		sgt.value = 20;
+		sgt2.value = 200;
+		System.out.println(sgt.value+"+"+ sgt2.value);
 	}
 }
